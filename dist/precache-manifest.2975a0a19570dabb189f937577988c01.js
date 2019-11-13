11af012d51450e73a84a5b70189a1d086a6dafdf
Dominika Miodowska <dominika.miodowska@ongoing.ch>
2019-11-13 16:51:06 +0100

    add functionalit server

---
+++ dist/precache-manifest.2975a0a19570dabb189f937577988c01.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520166f2bc933bfaa6ad4c89e0658b17",
+    "url": "./index.html"
+  },
+  {
+    "revision": "53f98d62d096316a189e",
+    "url": "main.css"
+  },
+  {
+    "revision": "53f98d62d096316a189e",
+    "url": "main.js"
+  }
+]);
